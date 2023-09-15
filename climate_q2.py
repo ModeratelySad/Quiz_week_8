d457432dd83f0af660c4587668e11a769346bf57
Jakob <jakobkarl@griffithuni.edu.au>
2023-09-15 20:23:48 +1000

    climate_q2

--- climate_q2.py
+++ climate_q2.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Load data from the CSV file
-data = pd.read_csv('climate.csv')  # Replace 'climate.csv' with the actual file path
+data = pd.read_csv('climate.csv')
 
-# Extract years, CO2, and temperature data from the DataFrame
 years = data['Year']
 co2 = data['CO2']
 temp = data['Temperature']
 
-# Create the figure and subplots
 plt.figure(figsize=(10, 6))
 
 plt.subplot(2, 1, 1)
@@ -23,6 +20,6 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)")
 
-plt.tight_layout()  # Ensures that the subplots do not overlap
+plt.tight_layout()
 plt.savefig("co2_temp_2.png")
 plt.show()
